9aedac4b16d273ef905f697ea528b2279b6cfe22
mowq <1034988215@qq.com>
2018-01-22 14:30:50 +0800

    同时发到多个队列中

--- mq-test/src/main/java/com/mo/activemq/queue/QueueReceiver.java
+++ mq-test/src/main/java/com/mo/activemq/queue/QueueReceiver.java
@@ -4,31 +4,48 @@ import org.apache.activemq.ActiveMQConnectionFactory;
 import javax.jms.*;
 
 public class QueueReceiver {
-    public static void main(String[] args) throws Exception{
 
-        String brokerURL = "tcp://122.112.217.219:61616";
-        //String brokerURL = "tcp://localhost:61617";
-
-        ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(brokerURL);
-
-        Connection connection = connectionFactory.createConnection();
-        connection.start();
-
-        final Session session = connection.createSession(Boolean.TRUE, Session.AUTO_ACKNOWLEDGE);
-        Destination destination = session.createQueue("my-queue");
-
-        MessageConsumer consumer = session.createConsumer(destination);
-
-        int i=0;
-        while (i<5){
-            i++;
-            TextMessage message = (TextMessage) consumer.receive(1000L);
-            if (message==null) break;
-            session.commit();//commit后，自动签收，下次就不会再收到这条消息了
-            System.out.println("收到消息:"+message.getText());
-        }
-
-        session.close();
-        connection.close();
+    public static void main(String[] args) {
+        QueueReceiver receiver = new QueueReceiver();
+        receiver.receive("tcp://122.112.217.219:61616","que2",5);
     }
+
+    /**
+     *
+     * @param brokerURL 地址
+     * @param queueName 队列
+     * @param cout 消费消息条数
+     */
+    public void receive(String brokerURL,String queueName,int cout){
+        new Thread(() -> {
+            try {
+                ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(brokerURL);
+
+                Connection connection = connectionFactory.createConnection();
+                connection.start();
+
+                final Session session = connection.createSession(Boolean.TRUE, Session.AUTO_ACKNOWLEDGE);
+                Destination destination = session.createQueue(queueName);
+
+                MessageConsumer consumer = session.createConsumer(destination);
+
+                int i=0;
+                while (i<cout){
+                    i++;
+                    TextMessage message = (TextMessage) consumer.receive(1000L);
+                    if (message==null) break;
+                    session.commit();//commit后，自动签收，下次就不会再收到这条消息了
+                    System.out.println("收到消息:"+message.getText());
+                }
+
+                session.close();
+                connection.close();
+            } catch (JMSException e) {
+                e.printStackTrace();
+            }
+        }).start();
+
+    }
+
+
 }
--- mq-test/src/main/java/com/mo/activemq/queue/QueueSender.java
+++ mq-test/src/main/java/com/mo/activemq/queue/QueueSender.java
@@ -7,41 +7,57 @@ import javax.jms.*;
 public class QueueSender {
 
     public static void main(String[] args) throws Exception {
+        QueueSender queueSender = new QueueSender();
 
         //failover容错
         //failover:(uri1,uri2...)?key=value
         //这种协议用于随机的去选择一个地址去连接，如果连接失败了，会找其他的连接。
         //randomize使用随机连接，达到负载均衡的效果，默认为true
-        String brokerURL = "failover:(tcp://122.112.217.219:61616,tcp://122.112.217.219:61617)?randomize=true";
-        //String brokerURL = "tcp://122.112.217.219:61616";
-        //String brokerURL = "tcp://localhost:61616";
-        //1 连接activemq服务器
-        ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(brokerURL);
+        //queueSender.send("failover:(tcp://122.112.217.219:61616,tcp://122.112.217.219:61617)?randomize=true", "my-queue",10);
 
-        //2 创建连接，并启动
-        Connection connection = connectionFactory.createConnection();
-        connection.start();
+        //同时发往多个队列，逗号分隔
+        queueSender.send("tcp://122.112.217.219:61616","que1,que2",10);
+    }
 
-        //3 创建会话
-        Session session = connection.createSession(Boolean.TRUE, Session.AUTO_ACKNOWLEDGE);
+    /**
+     *
+     * @param brokerURL 地址
+     * @param queueName 队列
+     * @param count 发送消息条数
+     */
+    public void send(String brokerURL, String queueName,int count) {
+        new Thread(() -> {
+            try {
+                //1 连接activemq服务器
+                ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(brokerURL);
 
-        //4 创建目的地，消息发送的地址
-        Destination destination = session.createQueue("my-queue");
+                //2 创建连接，并启动
+                Connection connection = connectionFactory.createConnection();
+                connection.start();
 
-        //5 创建生产者，消息发送人
-        MessageProducer producer = session.createProducer(destination);
-        for (int i = 0; i < 10; i++) {
-            //6 创建消息，并发送
-            TextMessage textMessage = session.createTextMessage("message--" + i);
-            //Thread.sleep(1000);
-            producer.send(textMessage);
-        }
+                //3 创建会话
+                Session session = connection.createSession(Boolean.TRUE, Session.AUTO_ACKNOWLEDGE);
 
-        //7 清理
-        session.commit();
-        session.close();
-        connection.close();
+                //4 创建目的地，消息发送的地址
+                Destination destination = session.createQueue(queueName);
 
+                //5 创建生产者，消息发送人
+                MessageProducer producer = session.createProducer(destination);
+                for (int i = 0; i < count; i++) {
+                    //6 创建消息，并发送
+                    TextMessage textMessage = session.createTextMessage("message--" + i);
+                    //Thread.sleep(1000);
+                    producer.send(textMessage);
+                }
+
+                //7 清理
+                session.commit();
+                session.close();
+                connection.close();
+            } catch (JMSException e) {
+                e.printStackTrace();
+            }
+        }).start();
     }
 
 }
